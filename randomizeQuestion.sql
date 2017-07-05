4d9bf1771128375eaea79551d99c365bdf36b508
mvinet <mickaelvinet79@gmail.com>
2017-07-05 10:02:01 +0200

    Ajout script randomize sql

---
+++ randomizeQuestion.sql
@@ -0,0 +1,73 @@
+USE [Projet]
+GO
+/****** Object:  UserDefinedFunction [dbo].[randomizeQuestion]    Script Date: 07/05/2017 09:00:16 ******/
+SET ANSI_NULLS ON
+GO
+IF OBJECT_ID('dbo.Get_NewID') IS NOT NULL
+BEGIN
+	DROP VIEW Get_NewID
+END
+GO
+CREATE VIEW Get_NewID
+AS
+SELECT NEWID() AS MyNewID
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER function [dbo].[randomizeQuestion](@idTest as int) 
+returns @result table (
+	 idQuestion int not null, 
+	 enonce varchar(100) not null, 
+	 media varchar(100) null, 
+	 poids int not null
+)
+as 
+begin
+
+	declare @loop_section as cursor;
+	declare @loop_question as cursor;
+	
+	declare @nbQuestion as int;
+	declare @idTheme as int;
+	
+	declare @idQuestion as int;
+	declare @enonce as varchar(100);
+	declare @media as varchar(100);
+	declare @poids as int;
+	
+	set @loop_section = cursor for
+		select nbquestionsatirer, idTheme
+		from section_test
+		where idTest = @idTest
+	
+	open @loop_section
+		fetch next from @loop_section into @nbQuestion, @idTheme
+		
+		while @@FETCH_STATUS = 0
+		begin
+
+			set @loop_question = cursor for 
+				select top(@nbQuestion) q.idQuestion, q.enonce, q.media, q.poids 
+				FROM theme t, section_test s, question q
+				where s.idtheme = t.idtheme
+				and t.idtheme = q.idtheme
+				and s.idtest = @idTest
+				and s.idtheme = @idTheme
+				ORDER BY (SELECT [MyNewId] FROM Get_NewID)
+				
+			open @loop_question
+				fetch next from @loop_question into @idQuestion, @enonce, @media, @poids
+				
+				while @@FETCH_STATUS = 0
+				begin
+					insert into @result values (@idQuestion, @enonce, @media, @poids);
+					fetch next from @loop_question into @idQuestion, @enonce, @media, @poids
+				end
+			close @loop_question
+			fetch next from @loop_section into @nbQuestion, @idTheme
+		end
+	
+	close @loop_section
+	
+	return
+end
